6b128d802a9c727a9d9117f883517055c4ad046e
idanmel <idanmel@gmail.com>
2020-02-03 23:53:46 +0200

    Added single tournament endpoint + error handling

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,38 @@ def get_tournaments():
     return jsonify(tournaments)
 
 
+@app.route("/tournaments/<int:uid>", methods=["GET"])
+def get_tournament(uid):
+    t = Tournament.query.get_or_404(uid)
+    return jsonify({
+        "id": t.uid,
+        "name": t.name
+    })
+
+
 @app.route("/tournaments", methods=["POST"])
 def create_tournament():
     name = request.json["name"]
     t = Tournament(name=name)
     t.insert()
+    response = jsonify()
+    response.status_code = 201
+    response.headers["location"] = f"/tournaments/{t.uid}"
+    return response
+
+
+def error_handler(status_code=404, message=""):
     return jsonify({
-        "nice": "well done"
-    })
+        "error": status_code,
+        "message": message,
+    }), status_code
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return error_handler(404, "resource not found")
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return error_handler(500, "internal server error")

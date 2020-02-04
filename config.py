54513b4b95c0f25099463eafef20a245dabe8ef3
Idan Melamed <idanmel@gmail.com>
2020-02-04 14:14:18 +0200

    randomized the secret key

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.urandom(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

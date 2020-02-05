bdce56939a8de03b7e9aaa15bd6aea25b15e909a
idanmel <idanmel@gmail.com>
2020-02-05 22:26:08 +0200

    load environment variables correctly

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
-from dotenv import load_dotenv
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-load_dotenv()
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 
 class Config(object):
+    load_dotenv()
     SECRET_KEY = os.urandom(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
